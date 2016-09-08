4c9b81a389ac788111b9887b4409b4d77b7382c9
Arick Smith <kidwriter2020@gmail.com>
2016-09-08 12:42:57 -0600

    I made a app that taught us about scanners.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,9 +1,32 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController 
 {
+	private String Rodgers;
+	private Scanner readKeyboardInput;
+	
+	public StupidController()
+	{
+		Rodgers = "-Arick Smith";
+		readKeyboardInput = new Scanner(System.in);
+		
+	}
+
 	public void start()
 	{
-		System.out.println("This is slightly less stupid :D");
+		System.out.println("This is slightly less stupid :D?");
+		System.out.println(Rodgers);
+		askQuestions();
 	}
+	
+		
+	private void askQuestions()
+	{
+		System.out.println("What is your name?");
+		String userInput = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput);
+	}
+	
 }
